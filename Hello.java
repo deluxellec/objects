2d2e61c9c33c3de0e422264d0c767a20a603d584
Joshua Leu <robloxpackpokemon@gmail.com>
2025-08-27 09:30:07 -0500

    code

---
+++ Hello.java
@@ -0,0 +1,13 @@
+import java.net.URL;
+import javax.swing.ImageIcon;
+import javax.swing.JOptionPane;
+
+public class Hello
+{
+    public static void main(String[] args) throws Exception
+    {
+        URL imageLocation = new URL("https://tenor.com/view/agnes-tachyon-uma-musume-dancing-dance-jukebox-gif-7997437424071463202");
+        JOptionPane.showMessageDialog(null, "Hello", "Title", JOptionPane.PLAIN_MESSAGE,
+        new ImageIcon(imageLocation));
+    }
+}
