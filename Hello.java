acfbb1f675885a5b029f0c9e0d749fbaa2e4a12e
Joshua Leu <robloxpackpokemon@gmail.com>
2025-08-28 08:26:24 -0500

    code

--- Hello.java
+++ Hello.java
@@ -5,7 +5,7 @@ public class Hello
 {
     public static void main(String[] args) throws Exception
     {
-        ImageIcon imageLocation = new ImageIcon("https://media.tenor.com/view/agnes-tachyon-uma-musume-dancing-dance-jukebox-gif-7997437424071463202.gif");
+        ImageIcon imageLocation = new ImageIcon("https://fetchpik.com-tFf2mOjRhw.gif");
         JOptionPane.showMessageDialog(null, "Hello", "Title", JOptionPane.PLAIN_MESSAGE, imageLocation);
     }
 }
